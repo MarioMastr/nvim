e86fb13922936ed954900806978a24ac541776e6
MarioMastr <ashrmoha11@gmail.com>
2026-06-14 15:46:20 +0300

    fix daps and copilot

--- lua/custom/plugins/dev.lua
+++ lua/custom/plugins/dev.lua
@@ -63,6 +63,37 @@ dap.configurations.cpp = {
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
 
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({'n', 'v'}, '<Leader>dh',
+    function()
+        require('dap.ui.widgets').hover()
+    end
+)
+vim.keymap.set({'n', 'v'}, '<Leader>dp',
+    function()
+        require('dap.ui.widgets').preview()
+    end
+)
+vim.keymap.set('n', '<Leader>df',
+    function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.frames)
+    end
+)
+vim.keymap.set('n', '<Leader>ds',
+    function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.scopes)
+    end
+)
 
 -- lsp
 vim.lsp.enable("clangd")
@@ -70,6 +101,7 @@ vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("zls")
 vim.lsp.enable("gopls")
 vim.lsp.enable("copilot")
+vim.lsp.enable("sourcekit-lsp")
 
 vim.lsp.config.clangd = {
     keys = {
@@ -107,6 +139,10 @@ vim.lsp.config.clangd = {
     }
 }
 
+vim.lsp.config.sourcekit_lsp = {
+    filetypes = { 'swift', 'objc', 'objcpp'}
+}
+
 vim.lsp.codelens.enable(true)
 vim.lsp.inlay_hint.enable(true)
 
@@ -114,13 +150,6 @@ vim.cmd[[set completeopt+=menuone,noselect,popup]]
 
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
-        vim.lsp.completion.enable(true, args.data.client_id, args.buf, {
-            autotrigger = true,
-            convert = function(item)
-            return { abbr = item.label:gsub('%b()', '') }
-            end,
-        })
-
         local bufnr = args.buf
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
@@ -139,6 +168,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
             vim.lsp.inline_completion.select,
             { desc = 'LSP: switch inline completion', buffer = bufnr }
         )
+
+        vim.lsp.completion.enable(true, args.data.client_id, args.buf, {
+            autotrigger = true,
+            convert = function(item)
+            return { abbr = item.label:gsub('%b()', '') }
+            end,
+        })
+
         end
     end,
 })
